aa93678004cfaf578d4b3e8bf6faf569889adcc2
Angga M. Ginanjar <36956295+supermenkw@users.noreply.github.com>
2020-07-25 00:04:27 +0700

    remove my own api key

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    KEY: 'ed6e01bb46e12182d690192ac29495f4',
+    KEY: YOUR_OWN_TMDB_API_KEY,
     BASE_URL: 'https://api.themoviedb.org/3/',
     BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
     DEFAULT_LANGUAGE: 'en-us',
