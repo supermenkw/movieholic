927e034ff05a9c78d71ee86f2d1713487a4ecd62
Angga M. Ginanjar <36956295+supermenkw@users.noreply.github.com>
2020-07-24 23:44:05 +0700

    init 🏁

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    KEY: 'ed6e01bb46e12182d690192ac29495f4',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
